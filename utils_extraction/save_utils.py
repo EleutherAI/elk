5026882c3fae686dd77f2456ccfca1c401911864
KayKozaronek <kozaronek@gmail.com>
2023-01-26 11:28:03 -0800

    rename save_csv to safe_df_to_csv function

--- utils_extraction/save_utils.py
+++ utils_extraction/save_utils.py
@@ -7,8 +7,8 @@ def save_params(args, name, coef, intercept):
     np.save(os.path.join(path, "coef_{}.npy".format(name)), coef)
     np.save(os.path.join(path, "intercept_{}.npy".format(name)), intercept)
 
-def save_csv(args, csv, prefix, str = ""):
+def save_df_to_csv(args, df, prefix, str = ""):
     dir = os.path.join(args.save_dir, "{}_{}_{}.csv".format(args.model, prefix, args.seed))
-    csv.to_csv(dir, index = False)
+    df.to_csv(dir, index = False)
     print("{} Saving to {} at {}".format(str, dir, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
 
