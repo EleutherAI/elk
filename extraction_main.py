6faaaf2e652fb222ba94ab4e0f3f19299f26c69c
Walter Laurito <Lauritowal@yahoo.com>
2023-01-28 07:07:37 +0100

    make setting mode more legible & remove unncessary vars

--- extraction_main.py
+++ extraction_main.py
@@ -15,9 +15,11 @@ import random
 
 
 if __name__ == "__main__":
-
     args = get_extraction_args(json_dir = "./registration")
+    print(f"-------- args = {args} --------")
+
     dataset_names = args.datasets
+    
     if not os.path.exists(args.save_dir):
         os.mkdir(args.save_dir)
     if not os.path.exists(os.path.join(args.save_dir, "params")):
@@ -30,10 +32,6 @@ if __name__ == "__main__":
         random.seed(args.seed)
         np.random.seed(args.seed)
 
-        # shorten the name
-        model = args.model
-        num_data = args.num_data
-    
         # Start calculate numbers
         # std is over all prompts within this dataset
         if not args.append:
@@ -43,23 +41,23 @@ if __name__ == "__main__":
             stats_df = pd.read_csv(dir)
 
         for method in tqdm(args.method_list, desc='Iterating over classification methods:', position=1, leave=False):
-            print("-------- method = {method} --------")
+            print(f"-------- method = {method} --------")
 
-            # TODO: Write this more elegantly
-            mode = args.mode if args.mode != "auto" else (
-                "minus" if method != "Prob" else "concat"
-            )
+            mode = args.mode
+            if args.mode == "auto":
+                # overwrite mode if set to auto
+                mode = "concat" if method == "Prob" else "minus"
 
             #TODO: UNDERSTAND AND RENAME PERMUTATION DICT
             dataset_to_hiddenstates, permutation_dict = get_hiddenstates_and_permutations(
                 load_dir=args.load_dir,
-                mdl_name= model, 
+                mdl_name= args.model, 
                 all_datasets=dataset_names,
                 prefix = prefix,
                 location = args.location,
                 layer = args.layer,
                 mode = mode,
-                num_data = num_data
+                num_data = args.num_data
             )
             
             # TODO: WHY IS THE CONSTRUCTED LIKE THIS AND WHAT IS IT USED FOR?
@@ -88,7 +86,7 @@ if __name__ == "__main__":
                 avg_accuracy_std = np.mean([np.std(accuracies) for accuracies in dataset_to_accurary_per_prompt.values()]), 
                 avg_loss = np.mean([np.mean(losses) for losses in dataset_to_loss_per_prompt.values()])
 
-                stats_df = populate_stats_df(dataset_to_accurary_per_prompt, dataset_to_loss_per_prompt, stats_df, model, prefix, method, args, train_set, dataset_names)
+                stats_df = populate_stats_df(dataset_to_accurary_per_prompt, dataset_to_loss_per_prompt, stats_df, args.model, prefix, method, args, train_set, dataset_names)
 
             save_df_to_csv(args, stats_df, prefix, f"After finish {method}")
 
