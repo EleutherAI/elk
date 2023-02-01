d7cb946120fba1ef08ff89d5d5b54e03af9d6d6e
Walter Laurito <Lauritowal@yahoo.com>
2023-02-01 06:18:07 +0100

    change to normal

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-nohup python -u generation_main.py --model deberta-v2-xxlarge-mnli --datasets imdb --prefix confusion --model_device cuda --num_data 1000 &
+# run in background
+nohup python -u generation_main.py --model deberta-v2-xxlarge-mnli --datasets imdb --prefix normal --model_device cuda --num_data 1000 &
 ps -ax | grep generation_main.py
