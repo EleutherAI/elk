8fd77ebf58e45afc1317781717ce037fb2e8acef
Walter Laurito <Lauritowal@yahoo.com>
2023-02-02 04:56:15 +0100

    make it a package + cleanup files

--- generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# run in background
-nohup python -u generation_main.py --model deberta-v2-xxlarge-mnli --datasets imdb --prefix normal --model_device cuda --num_data 1000 &
-ps -ax | grep generation_main.py
--- train_evaluate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python train.py --model deberta-v2-xxlarge-mnli --prefix normal  --dataset imdb  --num_data 1000
-python evaluate.py --model deberta-v2-xxlarge-mnli --prefix normal  --dataset boolq  --num_data 1000
