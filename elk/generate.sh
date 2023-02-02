31500dff9108f0e7f163cc9b36bbe08f65acc581
Walter Laurito <Lauritowal@yahoo.com>
2023-02-02 06:33:14 +0100

    call module in generate directly

--- elk/generate.sh
+++ elk/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # run in background
-nohup python -mu elk.generation_main --model deberta-v2-xxlarge-mnli --datasets imdb --prefix normal --model_device cuda --num_data 1000 &
+nohup python -m elk.generation_main --model deberta-v2-xxlarge-mnli --datasets imdb --prefix normal --model_device cuda --num_data 10 &
 ps -ax | grep generation_main
