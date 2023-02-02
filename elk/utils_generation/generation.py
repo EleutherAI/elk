3a08919e298792f48ccb6cb0b6923125c32a26d7
neverix <nev@neverix.io>
2023-02-02 23:50:35 +0400

    Fix generation.py to be hopefully more general

--- elk/utils_generation/generation.py
+++ elk/utils_generation/generation.py
@@ -6,6 +6,13 @@ from .save_utils import saveArray
 from .save_utils import save_records_to_csv
 
 
+def extract_hidden_states(out, states_location):
+    """
+    Extract either `hidden_states` (BERT/GPT) or `{states_location}_hidden_states` (T5)
+    """
+    return out.get("hidden_states", out.get(f"{states_location}_hidden_states", None))
+
+
 def calculate_hidden_state(args, model, tokenizer, frame, mdl_name):
 
     apply_tokenizer = functools.partial(
@@ -33,45 +40,30 @@ def calculate_hidden_state(args, model, tokenizer, frame, mdl_name):
 
     for idx in range(len(frame)):
         # calculate the hidden states
-        if "T0" in mdl_name or "unifiedqa" in mdl_name or "t5" in mdl_name:
-            if args.states_location == "encoder":
-                ids_paired = [
-                    apply_tokenizer(getDataPoint(frame, idx, w)) for w in ["0", "1"]
-                ]
-                hidden_states_paired = [
-                    model(
-                        ids, labels=pad_answer, output_hidden_states=True
-                    ).encoder_hidden_states
-                    for ids in ids_paired
-                ]
-            else:
-                answer_token = [
-                    apply_tokenizer(w) for w in getDataPoint(frame, idx, "selection")
-                ]
-                # get the input_ids for candidates
-                input_ids = apply_tokenizer(getDataPoint(frame, idx, "null"))
+        if ("T0" in mdl_name or "unifiedqa" in mdl_name or "t5" in mdl_name) and args.states_location == "decoder":
+            answer_token = [
+                apply_tokenizer(w) for w in getDataPoint(frame, idx, "selection")
+            ]
+            # get the input_ids for candidates
+            input_ids = apply_tokenizer(getDataPoint(frame, idx, "null"))
 
-                # calculate the hidden states and take the layer `state_idx`
-                hidden_states_paired = [
-                    model(
-                        input_ids, labels=a, output_hidden_states=True
-                    ).decoder_hidden_states
-                    for a in answer_token
-                ]
-        elif "gpt" in mdl_name or "bert" in mdl_name:
+            # calculate the hidden states and take the layer `state_idx`
+            hidden_states_paired = [
+                model(
+                    input_ids, labels=a, output_hidden_states=True
+                ).decoder_hidden_states
+                for a in answer_token
+            ]
+        else:
             appender = " " + str(tokenizer.eos_token) if "gpt" in mdl_name else ""
             ids_paired = [
                 apply_tokenizer(getDataPoint(frame, idx, w) + appender)
                 for w in ["0", "1"]
             ]
-            # Notice that since gpt and bert only have either decoder or encoder,
-            # we don't need to specify which one to use.
             hidden_states_paired = [
-                model(ids, output_hidden_states=True).hidden_states
+                extract_hidden_states(model(ids, output_hidden_states=True), args.states_location)
                 for ids in ids_paired
             ]
-        else:
-            raise NotImplementedError(f"model {mdl_name} is not supported!")
 
         # extract the corresponding token
         for i in range(2):
