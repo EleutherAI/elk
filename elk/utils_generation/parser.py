634abe09085f34c9babaed2794a7c7e98501d234
neverix <nev@neverix.io>
2023-02-03 12:42:34 +0400

    This did not actually work
    The recent pathlib change broke things

--- elk/utils_generation/parser.py
+++ elk/utils_generation/parser.py
@@ -146,7 +146,7 @@ def get_args():
     )
     parser.add_argument(
         "--states_location",
-        type=Path,
+        type=str,
         default="null",
         choices=["encoder", "decoder", "null"],
         help=(
