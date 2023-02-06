86825e44c21aa1cb964eb94c43b3120ab64301b0
Walter Laurito <Lauritowal@yahoo.com>
2023-02-06 07:12:48 +0100

    fix: correct args in debugging and bash file

--- elk/extract.sh
+++ elk/extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # run in background
-nohup python -m elk.extraction_main model deberta-v2-xxlarge-mnli dataset imdb --prefix normal --device cuda --max-examples 1000 &
+nohup python -m elk.extraction_main deberta-v2-xxlarge-mnli imdb --prefix normal --device cuda --max-examples 1000 &
 ps -ax | grep extraction_main
