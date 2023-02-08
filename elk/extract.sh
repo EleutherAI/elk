d6f35b9fabd0242f678721499aad52758fc4e55e
Walter Laurito <Lauritowal@yahoo.com>
2023-02-08 02:57:58 +0100

    cleanup debug and extract file

--- elk/extract.sh
+++ elk/extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # run in background
-nohup python -m elk.extraction_main deberta-v2-xxlarge-mnli imdb --prefix normal --device cuda --max-examples 1000 &
+nohup python -m elk.extraction_main deberta-v2-xxlarge-mnli super_glue boolq --label-column label --device cuda --max-examples 1000 &
 ps -ax | grep extraction_main
