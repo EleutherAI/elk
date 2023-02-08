2eb8ef54baceecf65cabf8e2ab915fe0c395fc1d
Walter Laurito <Lauritowal@yahoo.com>
2023-02-08 02:58:07 +0100

    cleanup train file

--- elk/train_evaluate.sh
+++ elk/train_evaluate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python -m elk.train suspicious-wu deberta-v2-xxlarge-mnli imdb
+python -m elk.train suspicious-wu
 # python -m elk.evaluate deberta-v2-xxlarge-mnli boolq --prefix normal --num-data 1000
