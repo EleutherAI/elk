804edf84e0fa31801fca4eaa512c9ad976ff4645
Nora Belrose <belrose.nora@gmail.com>
2023-02-16 06:43:45 +0000

    Add size column to 'elk list'

--- elk/list.py
+++ elk/list.py
@@ -6,7 +6,7 @@ import json
 
 def list_runs(args):
     path = elk_cache_dir()
-    table = PrettyTable(["Date", "Model", "Dataset", "UUID"])
+    table = PrettyTable(["Date", "Model", "Dataset", "Size", "UUID"])
 
     # Trivial case
     if not path.exists():
@@ -23,8 +23,10 @@ def list_runs(args):
             run_args = json.load(f)
 
         date = datetime.fromtimestamp(timestamp).strftime("%X %x")
+        num_bytes = sum(f.stat().st_size for f in run.glob("**/*") if f.is_file())
+        size = f"{num_bytes / 1e9:.1f} GB"
         table.add_row(
-            [date, run_args["model"], " ".join(run_args["dataset"]), run.name]
+            [date, run_args["model"], " ".join(run_args["dataset"]), size, run.name]
         )
 
     print(f"Cached runs in \033[1m{path}\033[0m:")  # bold
