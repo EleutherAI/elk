3c8913e1b6e37e0450a98e674d2993524dcf00eb
Walter Laurito <lauritowal@yahoo.com>
2023-02-18 23:28:16 +0000

    cleanup writing to csv

--- elk/evaluation/evaluate.py
+++ elk/evaluation/evaluate.py
@@ -3,6 +3,7 @@ import csv
 import torch
 
 from elk.training.preprocessing import load_hidden_states, normalize
+
 from ..files import elk_cache_dir
 
 
@@ -22,17 +23,19 @@ def evaluate_reporters(args):
             reporter_root_path / "reporters.pt", map_location=args.device
         )
 
-        L = hiddens.shape[1]
+        transfer_eval = reporter_root_path / "transfer_eval"
+        transfer_eval.mkdir(parents=True, exist_ok=True)
 
-        statistics = []
+        L = hiddens.shape[1]
+        layers = list(hiddens.unbind(1))
+        layers.reverse()
+        csv_file = transfer_eval / f"{hidden_state_dir}.csv"
+
         for reporter in reporters:
             reporter.eval()
 
-            layers = list(hiddens.unbind(1))
-            layers.reverse()
-
-            with torch.no_grad():
-                for hidden_state in layers:
+            with torch.no_grad(), open(csv_file, "w") as f:
+                for layer_idx, hidden_state in enumerate(layers):
                     x0, x1 = hidden_state.to(args.device).float().chunk(2, dim=-1)
 
                     result = reporter.score(
@@ -41,25 +44,20 @@ def evaluate_reporters(args):
                     )
                     stats = [*result]
                     stats += [args.normalization, args.name, hidden_state_dir]
-                    statistics.append(stats)
 
-        cols = [
-            "layer",
-            "acc",
-            "cal_acc",
-            "auroc",
-            "normalization",
-            "name",
-            "hidden_states",
-        ]
+                    writer = csv.writer(f)
+                    if not csv_file.exists():
+                        # write column names only once
+                        cols = [
+                            "layer",
+                            "acc",
+                            "cal_acc",
+                            "auroc",
+                            "normalization",
+                            "name",
+                            "hidden_states",
+                        ]
+                        writer.writerow(cols)
+                    writer.writerow([L - layer_idx] + [stats])
 
-        transfer_eval = reporter_root_path / "transfer_eval"
-        transfer_eval.mkdir(parents=True, exist_ok=True)
-        with open(transfer_eval / f"{hidden_state_dir}.csv", "w") as f:
-            writer = csv.writer(f)
-            writer.writerow(cols)
-
-            for i, stats in enumerate(statistics):
-                writer.writerow([L - i] + [s for s in stats])
-
-        print("Evaluation done.")
+        print("Eval file generated: ", csv_file)
