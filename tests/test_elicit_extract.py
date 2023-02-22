9abf16523245270abab34ee69d0c9b1e493546d3
Kaivu Hariharan <kaivu@mit.edu>
2023-02-22 06:13:55 +0000

    testing again

--- tests/test_elicit_extract.py
+++ tests/test_elicit_extract.py
@@ -4,20 +4,15 @@ from random import Random
 import math
 import numpy as np
 import pytest
-import os 
+import os
+
 
 # test that running both elicit and extract works
 # uses os.system to run the commands similar to how they would be run in the terminal
 # tests that readme example works
-@pytest.mark.gpu
+@pytest.mark.cpu
 def test_elk_run():
     # run elicit - check that the command runs without error
-    os.system("elk elicit microsoft/deberta-v2-xxlarge-mnli imdb")
+    os.system("elk elicit microsoft/deberta-v2-xxlarge-mnli imdb --max_examples 1")
     # run extract - check that the command runs without error
-    os.system("elk extract microsoft/deberta-v2-xxlarge-mnli imdb")
-
-
-
-
-    
-    
+    os.system("elk extract microsoft/deberta-v2-xxlarge-mnli imdb --max_examples 1")
