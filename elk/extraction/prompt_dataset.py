5fcc7521ea27d04a0117a73eac6e37ebf1d949a6
Alex Mallen <atmallen8@gmail.com>
2023-03-10 06:33:35 +0000

    allow boolean label col; add boolq templates

--- elk/extraction/prompt_dataset.py
+++ elk/extraction/prompt_dataset.py
@@ -2,7 +2,7 @@ from ..math_util import stochastic_round_constrained
 from ..promptsource import DatasetTemplates
 from ..utils import assert_type, compute_class_balance, infer_label_column, undersample
 from dataclasses import dataclass
-from datasets import DatasetDict, load_dataset
+from datasets import DatasetDict, load_dataset, ClassLabel
 from numpy.typing import NDArray
 from random import Random
 from simple_parsing.helpers import field, Serializable
@@ -126,10 +126,14 @@ class PromptDataset(TorchDataset):
 
         # Enforce class balance if needed
         if cfg.balance:
-            self.active_split = undersample(self.active_split, self.rng, label_col)
+            self.active_split = undersample(
+                self.active_split, self.rng, self.num_classes, label_col
+            )
             self.class_fracs = np.ones(self.num_classes) / self.num_classes
         else:
-            class_sizes = compute_class_balance(self.active_split, label_col)
+            class_sizes = compute_class_balance(
+                self.active_split, self.num_classes, label_col
+            )
             self.class_fracs: NDArray[np.floating] = class_sizes / class_sizes.sum()
 
         # We use stratified sampling to create few-shot prompts that are as balanced as
@@ -238,6 +242,14 @@ class PromptDataset(TorchDataset):
     @property
     def num_classes(self) -> int:
         """Number of classes in the underlying dataset."""
-
-        # We piggyback on the ClassLabel feature type to get the number of classes
-        return self.active_split.features[self.label_column].num_classes
+        if isinstance(self.active_split.features[self.label_column], ClassLabel):
+            # We piggyback on the ClassLabel feature type to get the number of classes
+            return self.active_split.features[self.label_column].num_classes
+        elif self.active_split.features[self.label_column].dtype == "bool":
+            return 2
+        else:
+            raise ValueError(
+                f"Can't infer number of classes from label column "
+                f"{self.label_column} of type "
+                f"{self.active_split.features[self.label_column]}"
+            )
