a55b3de6e91793e7eb3361a7c810b67f6b23aed7
Nora Belrose <belrose.nora@gmail.com>
2023-03-25 02:31:55 +0000

    Switch to torch.testing.assert_close in EigenReporter test

--- tests/test_eigen_reporter.py
+++ tests/test_eigen_reporter.py
@@ -19,21 +19,21 @@ def test_eigen_reporter():
 
     # Check that the streaming mean is correct
     pos_mu, neg_mu = x_pos.mean(dim=(0, 1)), x_neg.mean(dim=(0, 1))
-    assert torch.allclose(reporter.pos_mean, pos_mu)
-    assert torch.allclose(reporter.neg_mean, neg_mu)
+    torch.testing.assert_close(reporter.pos_mean, pos_mu)
+    torch.testing.assert_close(reporter.neg_mean, neg_mu)
 
     # Check that the streaming covariance is correct
     pos_centroids, neg_centroids = x_pos.mean(dim=1), x_neg.mean(dim=1)
     expected_var = batch_cov(pos_centroids) + batch_cov(neg_centroids)
-    assert torch.allclose(reporter.intercluster_cov, expected_var)
+    torch.testing.assert_close(reporter.intercluster_cov, expected_var)
 
     # Check that the streaming invariance (intra-cluster variance) is correct
     expected_invariance = cov_mean_fused(x_pos) + cov_mean_fused(x_neg)
-    assert torch.allclose(reporter.intracluster_cov, expected_invariance)
+    torch.testing.assert_close(reporter.intracluster_cov, expected_invariance)
 
     # Check that the streaming negative covariance is correct
     cross_cov = (pos_centroids - pos_mu).mT @ (neg_centroids - neg_mu) / num_clusters
     cross_cov = cross_cov + cross_cov.mT
-    assert torch.allclose(reporter.contrastive_xcov, cross_cov)
+    torch.testing.assert_close(reporter.contrastive_xcov, cross_cov)
 
     assert reporter.n == num_clusters
