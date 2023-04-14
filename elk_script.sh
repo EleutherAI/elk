1e3e9278d523dd3c53f1c0fe613592872700794a
ss.waree <sethapun@princeton.edu>
2023-04-14 16:10:42 +0000

    more arithmetic templates

--- elk_script.sh
+++ elk_script.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_0
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_40
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50
