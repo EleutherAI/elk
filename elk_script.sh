eb3242e6a655c136e98589d3f655b8c9264b29b5
ss.waree <sethapun@princeton.edu>
2023-04-14 17:32:18 +0000

    fixed typo in templates

--- elk_script.sh
+++ elk_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to2
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to100
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to500
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1000
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --net ccs
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to2 --net ccs
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --net ccs
+
