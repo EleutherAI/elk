9f5759b52bd7862688db37fb2a0a17a193a11df6
Nora Belrose <belrose.nora@gmail.com>
2023-04-17 20:16:00 +0000

    Fix stupid CCS bug

--- elk/metrics/__init__.py
+++ elk/metrics/__init__.py
@@ -1,12 +1,13 @@
 from .calibration import CalibrationError, CalibrationEstimate
 from .eval import EvalResult, evaluate_preds, to_one_hot
-from .roc_auc import RocAucResult, roc_auc_ci
+from .roc_auc import RocAucResult, roc_auc, roc_auc_ci
 
 __all__ = [
     "CalibrationError",
     "CalibrationEstimate",
     "EvalResult",
     "evaluate_preds",
+    "roc_auc",
     "roc_auc_ci",
     "to_one_hot",
     "RocAucResult",
