762544388cca27b77784e1cee924c63383d88cbb
ss.waree <sethapun@princeton.edu>
2023-04-17 06:30:14 +0000

    more templates

--- elk_script.sh
+++ elk_script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to2
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --net ccs
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to2 --net ccs
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --net ccs
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_40
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50
+
+
+
 
