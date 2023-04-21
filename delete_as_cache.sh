83982fe9819f7b3126cf5cfeb9955a6ecfe4b741
ss.waree <sethapun@princeton.edu>
2023-04-21 19:39:29 +0000

    bash scripts for convenience

---
+++ delete_as_cache.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /mnt/ssd-1/spar/.cache/huggingface/datasets/generator
+
+rm -rf default-cc696bbf431be0ef
+rm -rf default-9aad86e6260e0224
+rm -rf default-6c7b5446231bc8f3
+rm -rf default-f4f7fd7709a429aa
+rm -rf default-1298923addb46b7c
+rm -rf default-2cd9b371a3199a8c
+rm -rf default-c2c72a09a52b895a
+rm -rf default-50870ccab6bd3f08
+rm -rf default-8573e7cf29ec5045
+rm -rf default-5d331b5f6f83d37c
+rm -rf default-d0df27692fe2526d
+rm -rf default-8142ec5b18d6b40c
+rm -rf default-1d5052bcc07eb42d
+rm -rf default-39fb23a0af48c452
