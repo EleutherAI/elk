d70e4e989e08041b6afdb1afa9205aa779a793b4
ss.waree <sethapun@princeton.edu>
2023-04-25 21:11:18 +0000

    inference notebook

--- as_experiment.sh
+++ as_experiment.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --net ccs
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --net ccs
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --net ccs
