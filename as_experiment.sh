5773d020a52994782e77c57f3672a9a93a88f7c0
ss.waree <sethapun@princeton.edu>
2023-04-26 03:50:59 +0000

    slurm fix

--- as_experiment.sh
+++ as_experiment.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --net ccs
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --net ccs
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --net ccs
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50
 elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50 --net ccs
