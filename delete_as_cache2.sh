7f9d75fd28590502a8fadb4eac6e13bc64434e2f
ss.waree <sethapun@princeton.edu>
2023-04-26 02:21:42 +0000

    fixed templates, delete cache script

---
+++ delete_as_cache2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /mnt/ssd-1/spar/.cache/huggingface/datasets/generator
+
+rm -rf default-4ed2d823c14272be
+rm -rf default-32e2ed238e490c70
+rm -rf default-e06e05f947ebaf3c
+rm -rf default-959fe9758e6f511c
+rm -rf default-4024918d996f9e2c
+rm -rf default-2a538d0838dec3df
+rm -rf default-89d36c3458b3233c
+rm -rf default-8ef9b0c6640fc0a0
