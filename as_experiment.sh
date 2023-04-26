1a2dbe9dadd1bd47ddeea4ba3eebfbaafe79a7ba
ss.waree <sethapun@princeton.edu>
2023-04-26 16:23:48 +0000

    output notebook

--- as_experiment.sh
+++ as_experiment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --num_gpus
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --net ccs --num_gpus
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --num_gpus
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --net ccs --num_gpus
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --num_gpus
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --net ccs --num_gpus
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50 --num_gpus
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50 --net ccs --num_gpus
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --net ccs --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --net ccs --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --net ccs --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50 --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50 --net ccs --num_gpus 1
