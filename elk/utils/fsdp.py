ea7694e10840fa9d2bee2c35146e0249376436d7
James Chua <chuajamessh@gmail.com>
2023-04-27 18:11:33 +0800

    make it 5

--- elk/utils/fsdp.py
+++ elk/utils/fsdp.py
@@ -249,7 +249,7 @@ def _worker(
                 out_queue.put(output_applied)
 
             # Indicate we're done with this dataset
-            time.sleep(1)
+            time.sleep(5)
             out_queue.put(None)
 
         # Clean up the FSDP process group
