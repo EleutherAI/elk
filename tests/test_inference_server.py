e813a64b34946297e4b52a590d544ad0f33233dd
James Chua <chuajamessh@gmail.com>
2023-04-27 17:47:10 +0800

    replicate it by 2

--- tests/test_inference_server.py
+++ tests/test_inference_server.py
@@ -56,3 +56,25 @@ def test_inference_server_fsdp():
     assert (
         type(outputs) == transformers.modeling_outputs.CausalLMOutputWithCrossAttentions
     )
+
+
+def test_inference_server_fsdp_rep():
+    model_str = "sshleifer/tiny-gpt2"
+    server = InferenceServer(
+        model_str=model_str, num_workers=2, fsdp=True, cpu_offload=True
+    )
+    print("Started inference server")
+    # encode this text into input ids
+    text = "Hello, my dog is cute"
+    input_ids = AutoTokenizer.from_pretrained(model_str).encode(
+        text, return_tensors="pt"
+    )
+    # Make sure we only pass the arguments that the model expects
+    inputs = dict(input_ids=input_ids)
+    # make the dict a dataset, while still making it a pytorch dataset
+    input_dataset = Dataset.from_list([inputs, inputs])
+    input_dataset.set_format(type="torch")
+    outputs = server.map(dataset=input_dataset, closure=lambda x: x)[0]
+    assert (
+        type(outputs) == transformers.modeling_outputs.CausalLMOutputWithCrossAttentions
+    )
