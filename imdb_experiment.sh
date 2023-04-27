ac5fe6d16681b66f73b183ba1f2f4d1958cebfa6
ss.waree <sethapun@princeton.edu>
2023-04-27 17:06:41 +0000

    mispellings rerun

--- imdb_experiment.sh
+++ imdb_experiment.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_0 --num_gpus 1
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_0 --net ccs --num_gpus 1
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5 --num_gpus 1
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5 --net ccs --num_gpus 1
 elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --num_gpus 1
 elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --net ccs --num_gpus 1
 elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20 --num_gpus 1
