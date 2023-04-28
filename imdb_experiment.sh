285119b9faaec2230a3ce61a38b9149203431490
Waree Sethapun <sethapun@princeton.edu>
2023-04-28 14:58:45 +0000

    cycles

--- imdb_experiment.sh
+++ imdb_experiment.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --num_gpus 1
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --net ccs --num_gpus 1
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20 --num_gpus 1
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20 --net ccs --num_gpus 1
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50 --num_gpus 1
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50 --net ccs --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_40 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_40 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_1 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_1 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_2 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_2 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_3 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_3 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30 --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30 --net ccs --num_gpus 2
