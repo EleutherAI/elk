5ea35c71ecaf446127ba27e25935e02f0d3c2a42
James Chua <chuajamessh@gmail.com>
2023-04-28 19:24:28 +0800

    mark gpu

--- tests/test_inference_server.py
+++ tests/test_inference_server.py
@@ -12,9 +12,14 @@ from elk.utils.concurrency_utils import map_threadpool
 from elk.inference_server.fsdp import InferenceServer
 
 
-def test_inference_server_normal():
+def test_inference_server_non_fsdp():
     model_str = "sshleifer/tiny-gpt2"
-    server = InferenceServer(model_str=model_str, num_workers=2)
+    server = InferenceServer(
+        model_str=model_str,
+        num_workers=2,
+        fsdp=FSDPOptions(fsdp_enabled=False),
+        min_gpu_mem=0,
+    )
     print("Started inference server")
     # encode this text into input ids
     text = "Hello, my dog is cute"
@@ -32,11 +37,12 @@ def test_inference_server_normal():
     )
 
 
-def test_inference_server_fsdp_propagates_error():
+def test_inference_server_propagates_error():
+    """Test that the server propagates errors from workers to the main process"""
     model_str = "sshleifer/tiny-gpt2"
     server = InferenceServer(
         model_str=model_str,
-        num_workers=1,
+        num_workers=2,
         fsdp=FSDPOptions(fsdp_enabled=False),
         min_gpu_mem=0,
     )
@@ -54,11 +60,15 @@ def test_inference_server_fsdp_propagates_error():
     with pytest.raises(TypeError, match="got an unexpected keyword argument"):
         outputs = server.map(dataset=input_dataset, closure=lambda x: x)
 
-
-def test_inference_server_fsdp_one():
+@pytest.mark.gpu
+def test_fsdp_same_result():
+    """Test that the results from fsdp are the same as the results from a single model"""
     model_str = "sshleifer/tiny-gpt2"
     server = InferenceServer(
-        model_str=model_str, num_workers=2, fsdp=True, cpu_offload=True
+        model_str=model_str,
+        num_workers=2,
+        fsdp=FSDPOptions(fsdp_enabled=True),
+        min_gpu_mem=0,
     )
     print("Started inference server")
     # encode this text into input ids
@@ -76,75 +86,9 @@ def test_inference_server_fsdp_one():
         type(outputs) == transformers.modeling_outputs.CausalLMOutputWithCrossAttentions
     )
 
-
-def test_inference_server_fsdp_other_map_imp():
-    model_str = "sshleifer/tiny-gpt2"
-    single_model = transformers.AutoModelForCausalLM.from_pretrained(model_str)
-    server = InferenceServer(
-        model_str=model_str, num_workers=2, fsdp=True, cpu_offload=True
-    )
-    print("Started inference server")
-    # encode this text into input ids
-    text_one = "Hello, my dog is cute"
-    text_two = "Hello world!"
-
-    input_ids_one = AutoTokenizer.from_pretrained(model_str).encode(
-        text_one, return_tensors="pt"
-    )
-    input_ids_two = AutoTokenizer.from_pretrained(model_str).encode(
-        text_two, return_tensors="pt"
-    )
-    # Make sure we only pass the input_ids that the model expects
-    # make the dict a dataset, while still making it a pytorch dataset
-    input_dataset = Dataset.from_list(
-        [{"input_ids": input_ids_one}, {"input_ids": input_ids_two}]
-    )
-    input_dataset.set_format(type="torch")
-    outputs = server.map(dataset=input_dataset, closure=lambda x: x)
-    assert len(outputs) == 2
-    first_output = outputs[0]
-    assert (
-        type(first_output)
-        == transformers.modeling_outputs.CausalLMOutputWithCrossAttentions
-    )
-    # assert that the first output's logits is equal
-    first_output_logits = first_output.logits
-    second_output_logits = outputs[1].logits
-    single_model_logits = single_model(input_ids=input_ids_one).logits
-    try:
-        assert torch.allclose(first_output_logits, single_model_logits, atol=1e-3)
-    except Exception:
-        print("First output logits not equal to single model logits")
-        assert torch.allclose(second_output_logits, single_model_logits, atol=1e-3)
-
-
-def test_inference_server_fsdp_limited():
-    model_str = "sshleifer/tiny-gpt2"
-    server = InferenceServer(
-        model_str=model_str, num_workers=2, fsdp=False, cpu_offload=True
-    )
-    print("Started inference server")
-    # encode this text into input ids
-    text = "Hello, my dog is cute"
-    input_ids = AutoTokenizer.from_pretrained(model_str).encode(
-        text, return_tensors="pt"
-    )
-    # Make sure we only pass the arguments that the model expects
-    inputs = dict(input_ids=input_ids)
-    # make the dict a dataset, while still making it a pytorch dataset
-    input_dataset = Dataset.from_list([inputs, inputs])
-    # limit the dataset to 1
-    input_dataset = input_dataset.select(range(1))
-    assert len(input_dataset) == 1
-    input_dataset.set_format(type="torch")
-    outputs = server.map(dataset=input_dataset, closure=lambda x: x)[0]
-    assert (
-        type(outputs) == transformers.modeling_outputs.CausalLMOutputWithCrossAttentions
-    )
-
-
+@pytest.mark.gpu
 def test_fsdp_multithreading():
-    # make items repeat input_ids 1, but with ascending number of repeats
+    """Test that fsdp works with multithreading"""
     _dicts = [{"input_ids": torch.tensor([[55] * i])} for i in range(1, 10)]
     items: list[Dataset] = [
         Dataset.from_dict({"input_ids": torch.tensor([[1] * i])}) for i in range(1, 10)
@@ -156,7 +100,10 @@ def test_fsdp_multithreading():
     # # make a server
     model_str = "sshleifer/tiny-gpt2"
     server = InferenceServer(
-        model_str=model_str, num_workers=2, fsdp=False, cpu_offload=True
+        model_str=model_str,
+        num_workers=2,
+        fsdp=FSDPOptions(fsdp_enabled=True),
+        min_gpu_mem=0,
     )
     # run the function .one on the server
     outputs_server = map_threadpool(
