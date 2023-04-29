67f902f7d6bec54067b9dc3ec8868906bd51a2aa
Waree Sethapun <sethapun@princeton.edu>
2023-04-29 06:21:30 +0000

    exp

---
+++ imdb_experiment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_0 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_0 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --net ccs --num_gpus 3
