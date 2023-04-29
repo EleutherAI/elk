c06aa893ba79657231898ca9d0738e5b90925aaa
Waree Sethapun <sethapun@princeton.edu>
2023-04-29 04:10:20 +0000

    imdb

--- imdb_experiment.sh
+++ imdb_experiment.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_10 --net ccs --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_20 --net ccs --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_50 --net ccs --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_5 --net ccs --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_40 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_40 --net ccs --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_1 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_1 --net ccs --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_2 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_2 --net ccs --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_3 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_3 --net ccs --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30 --num_gpus 2
-elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30 --net ccs --num_gpus 2
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_0 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_0 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_40 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_40 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_1 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_1 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_2 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_2 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_3 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_3 --net ccs --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/imdb_misspelled_30 --net ccs --num_gpus 3
