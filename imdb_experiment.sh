59330e09072229d9d5e8ac64acdb0db8bde57981
Waree Sethapun <sethapun@princeton.edu>
2023-04-30 01:53:26 +0000

    results

--- imdb_experiment.sh
+++ imdb_experiment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --num_gpus 3
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --num_gpus 3
-elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --num_gpus 3
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to1 --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to10 --num_gpus 1
