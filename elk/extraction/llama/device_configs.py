91d98d2b7820a22dff90920067fb29675ea7bec0
James Chua <chuajamessh@gmail.com>
2023-05-01 19:31:48 +0800

    allocate the device properly

--- elk/extraction/llama/device_configs.py
+++ elk/extraction/llama/device_configs.py
@@ -9,6 +9,7 @@ from elk.utils import select_usable_devices, instantiate_model
 if TYPE_CHECKING:
     from elk import Extract
 
+
 @dataclass
 class Llama65bDeviceConfig:
     first_device: str
@@ -34,11 +35,18 @@ def select_devices_or_llama_65b_configs(
         print(f"Will create {num_pairs} llama workers ")
         forty_gb = 42_949_672_960
         llama_workers_config = []
-        for i in range(num_pairs):
-            devices = select_usable_devices(num_gpus, min_memory=forty_gb)
-            llama_workers_config.append(
-                Llama65bDeviceConfig(first_device=devices[0], second_device=devices[1])
+        devices = select_usable_devices(num_gpus, min_memory=forty_gb)
+        # split the devices into pairs
+        configs = []
+        while len(configs) < num_pairs:
+            first_device = devices.pop()
+            second_device = devices.pop()
+            configs.append(
+                Llama65bDeviceConfig(
+                    first_device=first_device, second_device=second_device
+                )
             )
+
         return llama_workers_config
 
 
