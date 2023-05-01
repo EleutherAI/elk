2b8e4cbf4ed07f6ee1e0f5b70ba358eaeca1f2cf
Waree Sethapun <sethapun@princeton.edu>
2023-05-01 17:32:52 +0000

    checkpoint

--- transfer_exp2.sh
+++ transfer_exp2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-elk eval test-huggyllama/llama-13b/sethapun/imdb_misspelled_0/ccs huggyllama/llama-13b sethapun/arithmetic_2as_1to5 --num_gpus 1
-elk eval test-huggyllama/llama-13b/sethapun/imdb_misspelled_0/ccs huggyllama/llama-13b sethapun/arithmetic_2as_1to50 --num_gpus 1
-elk eval test-huggyllama/llama-13b/sethapun/imdb_misspelled_0/ccs huggyllama/llama-13b sethapun/arithmetic_2as_1to250 --num_gpus 1
-elk eval test-huggyllama/llama-13b/sethapun/imdb_misspelled_0/ccs huggyllama/llama-13b sethapun/arithmetic_2as_1to500 --num_gpus 1
-elk eval test-huggyllama/llama-13b/sethapun/imdb_misspelled_0/ccs huggyllama/llama-13b sethapun/arithmetic_2as_1to750 --num_gpus 1
+
+elk eval test-huggyllama/llama-13b/sethapun/arithmetic_2as_1to1000/ccs huggyllama/llama-13b sethapun/imdb_misspelled_0 --num_gpus 1
+elk eval test-huggyllama/llama-13b/sethapun/arithmetic_2as_1to1000/ccs huggyllama/llama-13b sethapun/imdb_misspelled_10 --num_gpus 1
+elk eval test-huggyllama/llama-13b/sethapun/arithmetic_2as_1to1000/ccs huggyllama/llama-13b sethapun/imdb_misspelled_30 --num_gpus 1
+elk eval test-huggyllama/llama-13b/sethapun/arithmetic_2as_1to1000/ccs huggyllama/llama-13b sethapun/imdb_misspelled_50 --num_gpus 1
