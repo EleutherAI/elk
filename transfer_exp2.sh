97cf1bae9a65036e04d2a5dba1f8d2d3cff987a2
Waree Sethapun <sethapun@princeton.edu>
2023-05-01 19:23:58 +0000

    checkpoint

--- transfer_exp2.sh
+++ transfer_exp2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-elk eval test-huggyllama/llama-13b/sethapun/arithmetic_2as_1to1000/ccs huggyllama/llama-13b sethapun/imdb_misspelled_0 --num_gpus 1
-elk eval test-huggyllama/llama-13b/sethapun/arithmetic_2as_1to1000/ccs huggyllama/llama-13b sethapun/imdb_misspelled_10 --num_gpus 1
-elk eval test-huggyllama/llama-13b/sethapun/arithmetic_2as_1to1000/ccs huggyllama/llama-13b sethapun/imdb_misspelled_30 --num_gpus 1
-elk eval test-huggyllama/llama-13b/sethapun/arithmetic_2as_1to1000/ccs huggyllama/llama-13b sethapun/imdb_misspelled_50 --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to50 --net ccs --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to250 --net ccs --num_gpus 1
+elk elicit huggyllama/llama-13b sethapun/arithmetic_2as_1to750 --net ccs --num_gpus 1
+
+
