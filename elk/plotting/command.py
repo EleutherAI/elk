181283f1ed3b05ba0074e025ceb8cc1645a17692
jon <jonathan.n@u.nus.edu>
2023-05-14 21:21:34 +0100

    code fix

--- elk/plotting/command.py
+++ elk/plotting/command.py
@@ -7,17 +7,20 @@ from elk.plotting.visualize import visualize_sweep
 
 @dataclass
 class Plot:
-    sweeps: list[Path] = field(default_factory=list)
+    sweep: list[str] = field(default_factory=list)
 
     def execute(self):
         sweeps_path = Path.home() / "elk-reporters" / "sweeps"
         # in sweeps_path find the most recent sweep
         sweep = max(sweeps_path.iterdir(), key=os.path.getctime)
-        if self.sweeps:
-            sweep = self.sweeps[0]
-        if len(self.sweeps) > 1:
+        if self.sweep:
+            sweep = sweeps_path / self.sweep[0]
+            if not sweep.exists():
+                print(f"No sweep with name {self.sweep[0]} found in {sweeps_path}")
+                return
+        if len(self.sweep) > 1:
             print(
-                f"""{len(self.sweeps)} paths specified.
+                f"""{len(self.sweep)} paths specified.
                 Only one sweep is supported at this time."""
             )
         else:
