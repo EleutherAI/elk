9d6552f2b89abab3ea2d1eee776d884d4aaaa74c
jon <jonathan.n@u.nus.edu>
2023-05-18 19:25:34 +0100

    Fix duplicate bug

--- elk/plotting/command.py
+++ elk/plotting/command.py
@@ -1,9 +1,15 @@
 from dataclasses import dataclass
 
+import rich
+from rich.panel import Panel
 from simple_parsing import field
 
 from ..files import sweeps_dir
 from .visualize import visualize_sweep
+
+
+def pretty_error(msg):
+    rich.print(Panel(f"[red]Error[/red]: {msg}"))
 
 
 @dataclass
@@ -13,16 +19,19 @@ class Plot:
     def execute(self):
         sweeps_root_dir = sweeps_dir()
         sweep = max(sweeps_root_dir.iterdir(), key=lambda f: f.stat().st_ctime)
-        if self.sweeps:
-            sweep = sweeps_root_dir / self.sweeps[0]
-            if not sweep.exists():
-                print(f"No sweep with name {self.sweeps[0]} found in {sweeps_root_dir}")
-                return
-        if len(self.sweeps) > 1:
+        if self.sweeps and not (sweeps_root_dir / self.sweeps[0]).exists():
+            pretty_error(
+                f"No sweep with name {{{self.sweeps[0]}}} found in {sweeps_root_dir}"
+            )
+        elif len(self.sweeps) > 1:
             # TODO support more than one sweep
-            print(
+            pretty_error(
                 f"""{len(self.sweeps)} paths specified.
                 Only one sweep is supported at this time."""
+            )
+        elif (sweep / "viz").exists():
+            pretty_error(
+                f"[blue]{sweep / 'viz'}[/blue] already exists. Delete it to re-run."
             )
         else:
             visualize_sweep(sweep)
