fb8c3cf9430d149c8752b5734db0c4591819503f
jon <jonathan.n@u.nus.edu>
2023-05-22 22:44:12 +0100

    Remove unittest on test

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -1,31 +1,31 @@
-import unittest
 from pathlib import Path
 
-from pyfakefs.fake_filesystem_unittest import TestCase
+import pytest
 
 from elk.plotting.visualize import SweepVisualization
 
 
-class TestGetModelPaths(TestCase):
-    def setUp(self):
-        self.setUpPyfakefs()
+@pytest.fixture
+def setup_fs(fs):
+    test_dir = "/sweep1"
+    fs.create_dir(test_dir)
+    fs.create_dir(f"{test_dir}/huggyllama/llama-13b/imdb")
+    fs.create_file(f"{test_dir}/huggyllama/llama-13b/imdb/eval.csv")
+    fs.create_dir(f"{test_dir}/huggyllama/llama-12b/news")
+    fs.create_file(f"{test_dir}/huggyllama/llama-12b/news/eval.csv")
+    fs.create_file(f"{test_dir}/gpt2-medium/imdb/eval.csv")
 
-        # Create a test directory structure
-        self.test_dir = "/sweep1"
-        self.fs.create_dir(self.test_dir)
-        self.fs.create_dir(f"{self.test_dir}/huggyllama/llama-13b/imdb")
-        self.fs.create_file(f"{self.test_dir}/huggyllama/llama-13b/imdb/eval.csv")
-        self.fs.create_dir(f"{self.test_dir}/huggyllama/llama-12b/news")
-        self.fs.create_file(f"{self.test_dir}/huggyllama/llama-12b/news/eval.csv")
-        self.fs.create_file(f"{self.test_dir}/gpt2-medium/imdb/eval.csv")
-
-    def test_get_model_paths(self):
-        result = SweepVisualization._get_model_paths(Path(self.test_dir))
-        self.assertEqual(len(result), 3)
-        self.assertTrue(any([p.name == "llama-13b" for p in result]))
-        self.assertTrue(any([p.name == "llama-12b" for p in result]))
-        self.assertTrue(any([p.name == "gpt2-medium" for p in result]))
+    return Path(test_dir)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_get_model_paths(setup_fs):
+    test_dir = setup_fs
+    result = SweepVisualization._get_model_paths(test_dir)
+
+    root = Path(test_dir)
+    for path in root.rglob("*"):
+        print(path)
+    assert len(result) == 3
+    assert any([p.name == "llama-13b" for p in result])
+    assert any([p.name == "llama-12b" for p in result])
+    assert any([p.name == "gpt2-medium" for p in result])
