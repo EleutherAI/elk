8ebba01c11efa3c84056e2b2c78ce59408070958
Nora Belrose <belrose.nora@gmail.com>
2023-05-22 19:03:18 +0000

    Actually use self.sweeps

--- elk/plotting/command.py
+++ elk/plotting/command.py
@@ -1,5 +1,6 @@
 import shutil
 from dataclasses import dataclass
+from pathlib import Path
 
 import rich
 from rich.panel import Panel
@@ -15,12 +16,18 @@ def pretty_error(msg):
 
 @dataclass
 class Plot:
-    sweeps: list[str] = field(positional=True, default_factory=list)
+    sweeps: list[Path] = field(positional=True, default_factory=list)
     overwrite: bool = False
 
     def execute(self):
         sweeps_root_dir = sweeps_dir()
-        sweep = max(sweeps_root_dir.iterdir(), key=lambda f: f.stat().st_ctime)
+        if not self.sweeps:
+            self.sweeps = [
+                max(sweeps_root_dir.iterdir(), key=lambda f: f.stat().st_ctime)
+            ]
+        else:
+            self.sweeps = [sweeps_root_dir / sweep for sweep in self.sweeps]
+
         if self.sweeps and not (sweeps_root_dir / self.sweeps[0]).exists():
             pretty_error(
                 f"No sweep with name {{{self.sweeps[0]}}} found in {sweeps_root_dir}"
@@ -31,12 +38,12 @@ class Plot:
                 f"""{len(self.sweeps)} paths specified.
                 Only one sweep is supported at this time."""
             )
-        elif (sweep / "viz").exists() and not self.overwrite:
+        elif (self.sweeps[0] / "viz").exists() and not self.overwrite:
             pretty_error(
-                f"[blue]{sweep / 'viz'}[/blue] already exists. "
+                f"[blue]{self.sweeps[0] / 'viz'}[/blue] already exists. "
                 f"Use --overwrite to overwrite."
             )
         else:
             if self.overwrite:
-                shutil.rmtree(sweep / "viz")
-            visualize_sweep(sweep)
+                shutil.rmtree(self.sweeps[0] / "viz")
+            visualize_sweep(self.sweeps[0])
