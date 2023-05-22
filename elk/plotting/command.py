fdd5fabc9efa224a43c1c6eccb290a40d16787f5
jon <jonathan.n@u.nus.edu>
2023-05-22 23:12:26 +0100

    Annotated command dataclass, added comments, trivial refactor

--- elk/plotting/command.py
+++ elk/plotting/command.py
@@ -11,35 +11,40 @@ from .visualize import visualize_sweep
 
 
 def pretty_error(msg):
+    """Prints a pretty error message."""
     rich.print(Panel(f"[red]Error[/red]: {msg}"))
 
 
 @dataclass
 class Plot:
     sweeps: list[Path] = field(positional=True, default_factory=list)
+    """Names of the sweeps to plot. If empty, the most recent sweep is used."""
+
     overwrite: bool = False
+    """Whether to overwrite existing plots."""
 
     def execute(self):
         sweeps_root_dir = sweeps_dir()
-        if not self.sweeps:
-            self.sweeps = [
-                max(sweeps_root_dir.iterdir(), key=lambda f: f.stat().st_ctime)
-            ]
-        else:
-            self.sweeps = [sweeps_root_dir / sweep for sweep in self.sweeps]
+        # If sweep is nonempty, get the paths to the specified sweeps.
+        # If no sweep is specified, use the most recent one.
+        sweep_paths = (
+            [sweeps_root_dir / sweep for sweep in self.sweeps]
+            if self.sweeps
+            else [max(sweeps_root_dir.iterdir(), key=lambda f: f.stat().st_ctime)]
+        )
 
-        for sweep in self.sweeps:
-            if not (sweeps_root_dir / sweep).exists():
+        for sweep_path in sweep_paths:
+            if not sweep_path.exists():
                 pretty_error(
-                    f"No sweep with name {{{sweep}}} found in {sweeps_root_dir}"
+                    f"No sweep with name {{{sweep_path}}} found in {sweeps_root_dir}"
                 )
-            elif (sweep / "viz").exists() and not self.overwrite:
+            elif (sweep_path / "viz").exists() and not self.overwrite:
                 pretty_error(
-                    f"[blue]{sweep / 'viz'}[/blue] already exists. "
+                    f"[blue]{sweep_path / 'viz'}[/blue] already exists. "
                     f"Use --overwrite to overwrite."
                 )
             else:
                 if self.overwrite:
-                    shutil.rmtree(sweep / "viz")
+                    shutil.rmtree(sweep_path / "viz")
 
-                visualize_sweep(sweep)
+                visualize_sweep(sweep_path)
