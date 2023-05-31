639e0b356372cec9c5f7243663650ea0c765bbc5
jon <jonathan.n@u.nus.edu>
2023-05-31 14:34:30 +0000

    running the negation script

--- elk/extraction/neel.py
+++ elk/extraction/neel.py
@@ -1,9 +1,11 @@
 import os
 import random
+import time
 from dataclasses import dataclass
+from sys import argv
 
 import openai
-from datasets import load_dataset
+from datasets import IterableDatasetDict, load_dataset
 from rich import print
 from tqdm import tqdm
 
@@ -108,23 +110,25 @@ def get_neel_examples():
 
 def get_and_save_neel_inverted_by_lm():
     # Load dataset
-    dataset = load_dataset("NeelNanda/counterfact-tracing")
+    dataset: IterableDatasetDict = load_dataset("NeelNanda/counterfact-tracing")
 
     # Set up the OpenAI API
     openai.api_key = os.environ["OPENAI_API_KEY"]
     print("OpenAI API key:", openai.api_key)
 
     # Open the file in write mode
-    with open("inverted_prompts.tsv", "w") as f:
-        # Write the header
-        f.write(
-            "relation_id\toriginal_prompt\toriginal_target_true\t"
-            "original_target_false\tinverted_prompt_true\tinverted_prompt_false\n"
-        )
+    FILE_PATH = "inverted_prompts.tsv"
+    with open(FILE_PATH, "a") as f:
+        # Write the header if it exists
+        if os.path.getsize(FILE_PATH) == 0:
+            f.write(
+                "relation_id\toriginal_prompt\toriginal_target_true\t"
+                "original_target_false\tinverted_prompt_true\tinverted_prompt_false\n"
+            )
 
         # Iterate through the dataset and generate inverted prompts
-        start = 0
-        end = 5000  # len(dataset['train'])
+        start = int(argv[1])
+        end = int(argv[2])  # len(dataset['train'])
         bar = tqdm(range(start, end))
         for i in bar:
             data = dataset["train"][i]
@@ -168,19 +172,36 @@ def generate_inverted_prompt(prompt):
         "Autonomous University of Madrid, which not located in England"
     )
     prompt2 = prompt
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt1},
-            {"role": "assistant", "content": ans1},
-            {"role": "user", "content": prompt2},
-        ],
-    )
-    # Extract the generated inverted prompt from the API response
-    inverted_prompt = response.choices[0].message.content.strip()
-
-    return inverted_prompt
+    TRY_LIMIT = 3
+    for try_no in range(TRY_LIMIT):
+        start_time = time.time()
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": prompt1},
+                    {"role": "assistant", "content": ans1},
+                    {"role": "user", "content": prompt2},
+                ],
+            )
+            req_time = time.time() - start_time
+            print(f"Request time: {req_time}")
+            # Extract the generated inverted prompt from the API response
+            inverted_prompt = response.choices[0].message.content.strip()
+        except Exception as err:
+            if try_no < TRY_LIMIT:
+                req_time = time.time() - start_time
+                print(
+                    f"Error try {try_no}, stack trace: {err},"
+                    "took {req_time},\nRetrying..."
+                )
+                continue
+            else:
+                print(f"Error try {try_no}, stack trace: {err}\nSorry, goodbye.")
+                raise err
+        else:
+            return inverted_prompt
 
 
 if __name__ == "__main__":
