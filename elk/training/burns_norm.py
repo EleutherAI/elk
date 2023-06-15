b22d15562601f0c10600216d5717ff71cba75328
Walter Laurito <lauritowal@yahoo.com>
2023-06-15 13:12:55 +0200

    cleanup class and add annotation

--- elk/training/burns_norm.py
+++ elk/training/burns_norm.py
@@ -3,15 +3,12 @@ from torch import Tensor, nn
 
 
 class BurnsNorm(nn.Module):
-    """ Burns et al. style normalization  """
+    """ Burns et al. style normalization  Minimal changes from the original code. """
 
-    def forward(self, x: Tensor) -> Tensor:
-        breakpoint()
-        assert x.dim() == 3, "the input should have a dimension of 3." # TODO: add info about needed shape
+    def forward(self, x: Tensor) -> Tensor:        
+        assert x.dim() == 3, f"the input should have a dimension of 3 not dimension {x.dim()}, shape of x: {x.shape}" 
 
-        print("Per Prompt Normalization...")
         x: Tensor = x - torch.mean(x, dim=0)
         norm = torch.linalg.norm(x, dim=2)
         avg_norm = torch.mean(norm)
         return x / avg_norm * torch.sqrt(torch.tensor(x.shape[2], dtype=torch.float32))
-
