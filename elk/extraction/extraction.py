edde38786e2028b2f21eac8b7b0e6d01e1bba671
AugustasMacijauskas <august.macijauskas@gmail.com>
2023-07-12 18:36:59 +0100

    Fix seed not exposed (#270)
    * Fixed bug with encoder only models not expecting "labels" argument in forward pass
    
    * Fixed type annotations
    
    * Fixed a bug with decoder-only models
    
    * Fixed data.seed not exposed

--- elk/extraction/extraction.py
+++ elk/extraction/extraction.py
@@ -192,6 +192,7 @@ def extract_hiddens(
         template_path=cfg.template_path,
         rank=rank,
         world_size=world_size,
+        seed=cfg.seed,
     )
 
     # Add one to the number of layers to account for the embedding layer
