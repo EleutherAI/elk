4fc5c6a7462eb95a06da2932672f2ef09bfa8cd6
Reagan Lee <reaganjlee@gmail.com>
2023-07-25 14:50:33 +0000

    fix sweep ds config bug

--- elk/training/sweep.py
+++ elk/training/sweep.py
@@ -22,7 +22,8 @@ def assert_models_exist(model_names):
 
 def assert_datasets_exist(dataset_names):
     for dataset_name in dataset_names:
-        get_dataset_config_info(dataset_name)
+        ds_name, _, config_name = dataset_name.partition(":")
+        get_dataset_config_info(ds_name, config_name=config_name)
 
 
 @dataclass
