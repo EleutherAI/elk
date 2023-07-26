accee476ecdc35bd64a07e163c8123603a1abc42
jon <jonathan.n@u.nus.edu>
2023-07-26 14:57:16 +0000

    burns norm fix

--- elk/training/burns_norm.py
+++ elk/training/burns_norm.py
@@ -6,16 +6,9 @@ class BurnsNorm(nn.Module):
     """Burns et al. style normalization  Minimal changes from the original code."""
 
     def forward(self, x: Tensor) -> Tensor:
-        assert (
-            x.dim() == 3
-        ), f"the input should have a dimension of 3 not dimension {x.dim()}, \
-        current shape of input x: {x.shape}"
-
         x_mean: Tensor = x - torch.mean(x, dim=0)
-        norm = torch.linalg.norm(x_mean, dim=2)
-        avg_norm = torch.mean(norm)
-        return (
-            x_mean
-            / avg_norm
-            * torch.sqrt(torch.tensor(x_mean.shape[2], dtype=torch.float32))
+        std = torch.linalg.norm(x_mean, axis=0) / torch.sqrt(
+            torch.tensor(x_mean.shape[0], dtype=torch.float32)
         )
+        avg_norm = torch.mean(std)
+        return x_mean / avg_norm
