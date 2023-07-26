86b092308d0b9f490868377495276a13d808988b
jon <jonathan.n@u.nus.edu>
2023-07-26 16:13:48 +0000

    vectorized burns norm (teest me)

--- elk/training/burns_norm.py
+++ elk/training/burns_norm.py
@@ -6,13 +6,10 @@ class BurnsNorm(nn.Module):
     """Burns et al. style normalization  Minimal changes from the original code."""
 
     def forward(self, x_all: Tensor) -> Tensor:
-        res = []
-        xs = x_all.unbind(dim=1)
-        for x in xs:
-            x_mean: Tensor = x - x.mean(dim=0)
-            std = torch.linalg.norm(x_mean, axis=0) / torch.sqrt(
-                torch.tensor(x_mean.shape[0], dtype=torch.float32)
-            )
-            avg_norm = std.mean()
-            res.append(x_mean / avg_norm)
-        return torch.stack(res, dim=1)
+        assert x_all.ndim == 4
+        x_mean = x_all - x_all.mean(dim=0, keepdim=True)
+        std = torch.linalg.norm(x_mean, dim=0, keepdim=True) / torch.sqrt(
+            torch.tensor(x_mean.shape[0], dtype=torch.float32)
+        )
+        avg_norm = std.mean(dim=(0, 2, 3), keepdim=True)
+        return x_mean / avg_norm
