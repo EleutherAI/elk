ff800190f80178158ac627f3e53c8b797417ca03
Walter Laurito <Lauritowal@yahoo.com>
2023-07-26 21:26:53 +0000

    add scale flag

--- elk/training/burns_norm.py
+++ elk/training/burns_norm.py
@@ -4,15 +4,27 @@ from torch import Tensor, nn
 
 class BurnsNorm(nn.Module):
     """Burns et al. style normalization  Minimal changes from the original code."""
+    scale = False
 
     def forward(self, x_all: Tensor) -> Tensor:
-        res = []
-        xs = x_all.unbind(dim=1)
-        for x in xs:
-            x_mean: Tensor = x - x.mean(dim=0)
-            std = torch.linalg.norm(x_mean, axis=0) / torch.sqrt(
-                torch.tensor(x_mean.shape[0], dtype=torch.float32)
-            )
-            avg_norm = std.mean()
-            res.append(x_mean / avg_norm)
-        return torch.stack(res, dim=1)
+        if not self.scale:
+            # not dividing by std deviation (scaling). We just subtract the mean
+            return x_all - x_all.mean(dim=0, keep_dim=True)
+        else:
+            res = []
+
+            # x_all has the shape (n, v, d)
+            # unbind gives us a list of (n, d)
+            for x in x_all.unbind(dim=1):
+                x_normalized: Tensor = x - x.mean(dim=0)
+                
+                # the following code block is taken from the original Burns code 
+                # and uses a strange for calculating the std 
+                std = torch.linalg.norm(x_normalized, axis=0) / torch.sqrt(
+                    torch.tensor(x_normalized.shape[0], dtype=torch.float32)
+                )
+                avg_norm = std.mean()
+                res.append(x_normalized / avg_norm)
+
+            return torch.stack(res, dim=1) #  (n, v, d)
+    
