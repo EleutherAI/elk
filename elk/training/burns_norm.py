ad204865989b0083e957190edea1031f87c09bb2
jon <jonathan.n@u.nus.edu>
2023-07-26 15:33:57 +0000

    norm per prompt

--- elk/training/burns_norm.py
+++ elk/training/burns_norm.py
@@ -5,10 +5,14 @@ from torch import Tensor, nn
 class BurnsNorm(nn.Module):
     """Burns et al. style normalization  Minimal changes from the original code."""
 
-    def forward(self, x: Tensor) -> Tensor:
-        x_mean: Tensor = x - torch.mean(x, dim=0)
-        std = torch.linalg.norm(x_mean, axis=0) / torch.sqrt(
-            torch.tensor(x_mean.shape[0], dtype=torch.float32)
-        )
-        avg_norm = torch.mean(std)
-        return x_mean / avg_norm
+    def forward(self, x_all: Tensor) -> Tensor:
+        res = []
+        xs = x_all.unbind(dim=1)
+        for x in xs:
+            x_mean: Tensor = x - x.mean(dim=0)
+            std = torch.linalg.norm(x_mean, axis=0) / torch.sqrt(
+                torch.tensor(x_mean.shape[0], dtype=torch.float32)
+            )
+            avg_norm = std.mean()
+            res.append(x_mean / avg_norm)
+        return torch.stack(res, dim=1)
