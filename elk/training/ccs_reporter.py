add platt scaling for burns + fix for leace

elk/training/ccs_reporter.py
@@ -88,6 +88,8 @@ class CcsReporter(nn.Module, PlattMixin):
         num_variants: int = 1,
     ):
         super().__init__()
+        self._is_training = True
+
         self.config = cfg
         self.in_features = in_features
         self.num_variants = num_variants
@@ -164,15 +166,12 @@ class CcsReporter(nn.Module, PlattMixin):
     def forward(self, x: Tensor) -> Tensor:
         """Return the credence assigned to the hidden state `x`."""
         assert self.norm is not None, "Must call fit() before forward()"
-
         raw_scores = self.probe(self.norm(x)).squeeze(-1)
-        if self.config.norm == "leace":
-            return raw_scores.mul(self.scale).add(self.bias).squeeze(-1)
-
-        elif self.config.norm == "burns":
+        if self._is_training:
             return raw_scores
         else:
-            raise ValueError(f"Unknown normalization {self.config.norm}.")
+            platt_scaled_scores = raw_scores.mul(self.scale).add(self.bias).squeeze(-1)
+            return platt_scaled_scores
 
     def loss(self, logit0: Tensor, logit1: Tensor) -> Tensor:
         """Return the loss of the reporter on the contrast pair (x0, x1).
@@ -248,6 +247,8 @@ class CcsReporter(nn.Module, PlattMixin):
             raise RuntimeError("Got NaN/infinite loss during training")
 
         self.load_state_dict(best_state)
+
+        self._is_training = False
         return best_loss
 
     def train_loop_adam(self, x_neg: Tensor, x_pos: Tensor) -> float: